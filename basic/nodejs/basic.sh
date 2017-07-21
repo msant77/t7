b25e49aed58f604262c24fba5a43ecb44e1cf50a
msant77 <marcosantana@msn.com>
2017-07-21 17:30:58 -0400

    added nodejs basic install drill

--- basic/nodejs/basic.sh
+++ basic/nodejs/basic.sh
@@ -1,2 +1,3 @@
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+#https://nodejs.org/en/download/package-manager/
+curl --silent --location  https://rpm.nodesource.com/setup_8.x | sudo -E bash -
+sudo yum install -y nodejs
