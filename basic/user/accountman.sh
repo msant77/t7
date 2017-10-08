cf1ae04eb884ccfe3ee2362da46b23ad8bf60f7f
msant77 <marcosantana@msn.com>
2017-10-08 10:40:26 -0300

    add list all groups to usermanagement

--- basic/user/accountman.sh
+++ basic/user/accountman.sh
@@ -1,3 +1,6 @@
+#shows all groups
+getent group
+
 #change current password
 passwd
 
@@ -8,12 +11,16 @@ useradd <username>
 passwd <username> 
 
 #user mod
-usermod
+usermod -a -G exampleGroup exampleUser
+
+#add to sudo 
+usermod -a -G sudo username 
+
 
 #user removal 
 userdel <username>
 
 
 # chfn, chsh, crypt, groupadd, groupdel, groupmod, 
-# login.defs, newusers, subgid, subuid, 
+# login.defs, newusers, subgid, subuid, users, groups
 
