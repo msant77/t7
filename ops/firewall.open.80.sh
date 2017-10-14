7017c3c9e336082b2023265aca68791c7e34843c
msant77 <marcosantana@msn.com>
2017-10-14 15:29:55 -0400

    add firewall.open ports 80 and 8080 to ubuntu ops

---
+++ ops/firewall.open.80.sh
@@ -0,0 +1,8 @@
+#https://www.digitalocean.com/community/tutorials/iptables-essentials-common-firewall-rules-and-commands
+#https://www.digitalocean.com/community/tutorials/how-to-set-up-a-basic-iptables-firewall-on-centos-6
+
+
+#Allow All Incoming HTTP
+sudo iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
