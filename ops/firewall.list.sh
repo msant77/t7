ad9dabf1fd1ea9261c203f874c6f3ff867b855c5
msant77 <marcosantana@msn.com>
2017-10-14 15:28:04 -0400

    add ubuntu iptables firewall detailed list on ops

---
+++ ops/firewall.list.sh
@@ -0,0 +1,6 @@
+#https://www.digitalocean.com/community/tutorials/iptables-essentials-common-firewall-rules-and-commands
+#https://www.digitalocean.com/community/tutorials/how-to-set-up-a-basic-iptables-firewall-on-centos-6
+
+#List rules with line number and verbose 
+iptables --list --line-numbers
+
