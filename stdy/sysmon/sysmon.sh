7740844daa2820aa576282a77bde3022841c454a
msant77 <marcosantana@msn.com>
2019-01-01 21:38:01 -0200

    improve commands list for system monitoring

--- stdy/sysmon/sysmon.sh
+++ stdy/sysmon/sysmon.sh
@@ -9,5 +9,15 @@ ps -A
 #check and filter running processes by name 
 ps -A | grep <name to find>
 
+# list processes using a port
+# lsof -ti tcp:<port number>
 
+lsof -ti tcp:10010 
 
+# list processes based on name 
+ps -e | grep storeaccountd
+
+# give all information about a process 
+# launchctl print pid/<port number>
+
+launchctl print pid/10010
