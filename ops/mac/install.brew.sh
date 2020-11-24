618679454d1b5f078f9825bf818c9ced7c958221
msant77 <marcosantana@msn.com>
2020-11-23 22:26:17 -0300

    update brew version

--- ops/mac/install.brew.sh
+++ ops/mac/install.brew.sh
@@ -1,2 +1,1 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
+ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
